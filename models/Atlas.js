ca6504979c0e2946ebad745565fd2591d4fbae46
Nikithavedanth <nikithavedanth@gmail.com>
2022-11-09 17:20:24 -0600

    updated

--- models/Atlas.js
+++ models/Atlas.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const AtlasSchema = mongoose.Schema({
-name: String,
-state: String,
-stateranking: Number
+    name: String,
+    state: String,
+    stateranking: Number
 })
 module.exports = mongoose.model("Atlas",
-AtlasSchema)
+    AtlasSchema)
